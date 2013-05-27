b42447532b522477a759e78b8f3a5e836d8be431
Keith Young Johnson <keithjohnson@Keiths-MacBook-Pro.local>
2013-05-27 16:53:09 -0400

    adding tracks on click.  although needs toggling logic

--- app/assets/javascripts/views/album_view.js
+++ app/assets/javascripts/views/album_view.js
@@ -20,9 +20,37 @@ window.app.views.AlbumView = Backbone.View.extend({
   },
 
   showTracks: function(){
+    // this.model.tracks = new app.collections.TrackList();
+    // this.model.tracks.url = '/albums/' + this.model.id + '/tracks';
+    // tracks = this.model.tracks.fetch();
+    // console.log(tracks);
+    _this = this
     tracks = this.model.getTracks();
-    console.log(tracks);
+    tracks.forEach(function(track){
+        console.log(track);
+        console.log(track.attributes.title);
+        _this.$el.find('.album-info').append(track.attributes.title);
+ 
+    });
+    return this;
+  //   .forEach(function(track){
+  //     console.log(track);
+  //   });
+
   }
 
 });
 
+//    render: function() {
+//      this.$el.html(this.template({ project : this.model }));
+// +    var _this = this;
+// +    this.model.getSkills().forEach(function(skill) {
+// +      var skill_html = new app.views._Skill({
+// +        project: _this.model,
+// +        model: skill
+// +      });
+// +      _this.$el.find('.skill-list').append(skill_html.render().el);
+// +    });
+// +
+//      return this;
+//    },
